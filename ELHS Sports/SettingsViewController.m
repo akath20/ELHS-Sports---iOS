b9fd262502437a4b7d1e873dcea4f508e45169ae
snipper9 <akath20@charter.net>
2014-02-27 21:30:02 -0500

    Settings up the Settings page

---
+++ ELHS Sports/SettingsViewController.m
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.m
+//  ELHS Sports
+//
+//  Created by Alex Atwater on 2/27/14.
+//  Copyright (c) 2014 Alex Atwater. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+
+
+@end
