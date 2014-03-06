bf820c9c9c98a97249c7a7d2c537f8ff987ac12c
snipper9 <akath20@charter.net>
2014-03-05 20:56:45 -0500

    Fully Implemented the Home Screen Layout for all views for the iPad

--- ELHS Sports/ViewController.m
+++ ELHS Sports/ViewController.m
@@ -92,7 +92,7 @@
         }
     } else {
         //iPad
-        //[self iPadOrientationSetUp];
+        [self iPadOrientationSetUp];
     }
     
     
@@ -167,6 +167,64 @@
     
 }
 
+- (void)viewDidLayoutSubviews {
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        //iPhone
+        
+        [self.adBanner setFrame:CGRectMake(0, 64, 320, 50)];
+        
+        if (!((int)[[UIScreen mainScreen] bounds].size.height == 568)) {
+            
+            int magicNumber = 25;
+            int modNumber = 15;
+            float xMove  =.5;
+            
+            
+            
+            for (UIButton *currentButton in [self allButtons]) {
+                if (currentButton.tag == 1) {
+                    //top line
+                    [currentButton setFrame:CGRectMake(currentButton.frame.origin.x, (currentButton.frame.origin.y - magicNumber  + modNumber), (currentButton.frame.size.width - magicNumber), (currentButton.frame.size.height - magicNumber))];
+                } else if (currentButton.tag == 2) {
+                    //bottem line
+                    [currentButton setFrame:CGRectMake(currentButton.frame.origin.x, (currentButton.frame.origin.y - magicNumber - modNumber), (currentButton.frame.size.width - magicNumber), (currentButton.frame.size.height - magicNumber))];
+                } else {
+                    //middle line
+                    [currentButton setFrame:CGRectMake(currentButton.frame.origin.x, (currentButton.frame.origin.y - magicNumber), (currentButton.frame.size.width - magicNumber), (currentButton.frame.size.height - magicNumber))];
+                }
+                
+                [currentButton setFrame:CGRectMake((currentButton.frame.origin.x + (magicNumber * xMove)), currentButton.frame.origin.y, currentButton.frame.size.width, currentButton.frame.size.height)];
+                
+            }
+            
+            for (UILabel *currentLabel in [self allLabels]) {
+                
+                
+                
+                
+                if (currentLabel.tag == 1) {
+                    //top line
+                    [currentLabel setFrame:CGRectMake(currentLabel.frame.origin.x, (currentLabel.frame.origin.y - (magicNumber * 1.75) + (modNumber  * .75)), currentLabel.frame.size.width, currentLabel.frame.size.height)];
+                } else if (currentLabel.tag == 2) {
+                    //bottem line
+                    [currentLabel setFrame:CGRectMake(currentLabel.frame.origin.x, (currentLabel.frame.origin.y - (magicNumber * 1.75) - (modNumber  * .75)), currentLabel.frame.size.width, currentLabel.frame.size.height)];
+                } else {
+                    //middle line
+                    [currentLabel setFrame:CGRectMake(currentLabel.frame.origin.x, (currentLabel.frame.origin.y - (magicNumber * 1.75)), currentLabel.frame.size.width, currentLabel.frame.size.height)];
+                }
+                
+                
+                
+                
+            }
+            
+        }
+    } else {
+        //iPad
+        [self iPadOrientationSetUp];
+    }
+}
+
 - (void)viewDidDisappear:(BOOL)animated {
     [[NSNotificationCenter defaultCenter] removeObserver:self name:@"bannerLoaded" object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:@"bannerError" object:nil];
@@ -179,75 +237,103 @@
         //set the buttons
         for (UIButton *currentButton in self.allButtons) {
             if ([currentButton.titleLabel.text isEqualToString:@"Basketball"]) {
-                [currentButton setFrame:CGRectMake(94, 115, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(140, 153, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Soccer"]) {
-                [currentButton setFrame:CGRectMake(57, 431, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(46, 431, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Skiing"]) {
-                [currentButton setFrame:CGRectMake(72, 731, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(51, 760, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Wrestling"]) {
-                [currentButton setFrame:CGRectMake(305, 738, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(291, 763, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Track"]) {
-                [currentButton setFrame:CGRectMake(566, 757, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(559, 770, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Hockey"]) {
-                [currentButton setFrame:CGRectMake(566, 442, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(570, 453, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Swimming"]) {
-                [currentButton setFrame:CGRectMake(496, 118, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(469, 155, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Home"]) {
-                [currentButton setFrame:CGRectMake(273, 318, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(244, 375, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else {
                 NSLog(@"\nError. ViewController -> iPadOrientationSetUp, portrait, buttons");
             }
         }
+        
+        //set the labels
+        for (UILabel *currentLabel in self.allLabels) {
+            if ([currentLabel.text isEqualToString:@"Basketball"]) {
+                [currentLabel setFrame:CGRectMake(144, 324, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Soccer"]) {
+                [currentLabel setFrame:CGRectMake(68, 645, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Skiing"]) {
+                [currentLabel setFrame:CGRectMake(56, 908, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Wrestling"]) {
+                [currentLabel setFrame:CGRectMake(312, 915, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Track"]) {
+                [currentLabel setFrame:CGRectMake(585, 911, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Hockey"]) {
+                [currentLabel setFrame:CGRectMake(587, 646, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Swimming & Diving"]) {
+                [currentLabel setFrame:CGRectMake(458, 297, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Home"]) {
+                [currentLabel setFrame:CGRectMake(331, 656, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else {
+                NSLog(@"\nError. ViewController -> iPadOrientationSetUp, portrait, labels");
+            }
+        }
+        
+        
+        
+        
+        
     } else {
         //if landscape
         
         //set the buttons
         for (UIButton *currentButton in self.allButtons) {
             if ([currentButton.titleLabel.text isEqualToString:@"Basketball"]) {
-                [currentButton setFrame:CGRectMake(94, 115, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(110, 141, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Soccer"]) {
-                [currentButton setFrame:CGRectMake(57, 431, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(111, 372, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Skiing"]) {
-                [currentButton setFrame:CGRectMake(72, 731, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(123, 571, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Wrestling"]) {
-                [currentButton setFrame:CGRectMake(305, 738, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(419, 575, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Track"]) {
-                [currentButton setFrame:CGRectMake(566, 757, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(749, 581, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Hockey"]) {
-                [currentButton setFrame:CGRectMake(566, 442, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(760, 372, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Swimming"]) {
-                [currentButton setFrame:CGRectMake(496, 118, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(756, 141, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else if ([currentButton.titleLabel.text isEqualToString:@"Home"]) {
-                [currentButton setFrame:CGRectMake(273, 318, currentButton.frame.size.width, currentButton.frame.size.height)];
+                [currentButton setFrame:CGRectMake(372, 181, currentButton.frame.size.width, currentButton.frame.size.height)];
             } else {
-                NSLog(@"\nError. ViewController -> iPadOrientationSetUp, landscape, buttons");
+                NSLog(@"\nError. ViewController -> iPadOrientationSetUp, portrait, buttons");
             }
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
         //set the labels
         for (UILabel *currentLabel in self.allLabels) {
-            for (UIButton *currentButton in self.allButtons) {
-                if ([currentLabel.text isEqualToString:currentButton.titleLabel.text]) {
-                    [currentLabel setFrame:CGRectMake(currentButton.frame.origin.x, currentButton.frame.origin.y-26, currentLabel.frame.size.width, currentLabel.frame.size.height)];
-                } else if ([currentLabel.text isEqualToString:@"Swimming & Diving"]) {
-                    [currentLabel setFrame:CGRectMake(self.swimButton.frame.origin.x, self.swimButton.frame.origin.y - 26, currentLabel.frame.size.width, currentLabel.frame.size.height)];
-                }
+            if ([currentLabel.text isEqualToString:@"Basketball"]) {
+                [currentLabel setFrame:CGRectMake(114, 309, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Soccer"]) {
+                [currentLabel setFrame:CGRectMake(133, 514, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Skiing"]) {
+                [currentLabel setFrame:CGRectMake(137, 719, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Wrestling"]) {
+                [currentLabel setFrame:CGRectMake(440, 723, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Track"]) {
+                [currentLabel setFrame:CGRectMake(773, 719, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Hockey"]) {
+                [currentLabel setFrame:CGRectMake(777, 515, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Swimming & Diving"]) {
+                [currentLabel setFrame:CGRectMake(745, 282, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else if ([currentLabel.text isEqualToString:@"Home"]) {
+                [currentLabel setFrame:CGRectMake(459, 525, currentLabel.frame.size.width, currentLabel.frame.size.height)];
+            } else {
+                NSLog(@"\nError. ViewController -> iPadOrientationSetUp, portrait, labels");
             }
         }
+        
+
         
     }
     
