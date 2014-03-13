56accc96103b026235bc781f43540f8e31e069cd
snipper9 <akath20@charter.net>
2014-03-12 21:14:02 -0400

    First commit, new branch, redesign with table view

---
+++ ELHS Sports/MainMenuTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainMenuTableViewController.h
+//  ELHS Sports
+//
+//  Created by Alex Atwater on 3/12/14.
+//  Copyright (c) 2014 Alex Atwater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
+
+@interface MainMenuTableViewController : UITableViewController
+
+@property (strong, nonatomic) ADBannerView *adBanner;
+
+@end
---
+++ ELHS Sports/MainMenuTableViewController.m
@@ -0,0 +1,155 @@
+//
+//  MainMenuTableViewController.m
+//  ELHS Sports
+//
+//  Created by Alex Atwater on 3/12/14.
+//  Copyright (c) 2014 Alex Atwater. All rights reserved.
+//
+
+#import "MainMenuTableViewController.h"
+#import "SharedValues.h"
+#import "AppDelegate.h"
+#import "Reachability.h"
+
+@interface MainMenuTableViewController ()
+
+@end
+
+@implementation MainMenuTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
+    
+    
+    //check for internet connection
+    if (![Reachability checkForInternetWithString:Nil]) {
+        //show alert from the class
+        [[Reachability showAlertNoInternet] show];
+    }
+    
+    
+    
+    //The ad
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loadBanner) name:@"bannerLoaded" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(bannerError) name:@"bannerError" object:nil];
+    
+    self.adBanner = SharedAdBannerView;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 1;
+}
+
+
+- (void)loadBanner {
+    
+    
+    
+}
+
+- (void)bannerError {
+    
+    
+}
+
+
+
+
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+//    
+//    // Configure the cell...
+//    
+//    return cell;
+//}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
